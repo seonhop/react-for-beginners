50f1618de62b2dc943841c7abb0b9f77ef09947e
seonhop <78877741+seonhop@users.noreply.github.com>
2022-07-02 14:17:41 -0400

    #6.0 Introduction
    setup

--- react-for-beginners-app/src/App.js
+++ react-for-beginners-app/src/App.js
@@ -1,11 +1,14 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useState } from "react";
 
 function App() {
+    const [counter, setCounter] = useState(0);
+    const onClick = () => setCounter((prev) => prev + 1);
     return (
         <div>
-            <h1 className={styles.title}>Hi!!</h1>
-            <Button text="Click" />
+            <h1>{counter}</h1>
+            <button onClick={onClick}>Click me!</button>
         </div>
     );
 }
