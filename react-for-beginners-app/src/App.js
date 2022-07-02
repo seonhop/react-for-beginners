627bd3679336b40e0020851f53f6835ed64de19a
seonhop <78877741+seonhop@users.noreply.github.com>
2022-07-02 15:33:24 -0400

    #6.4 Cleanup
    #Cleanup function
    
    Return a function inside the function of useEffect to produce a cleanup function (catches if the element's been destroyed)

--- react-for-beginners-app/src/App.js
+++ react-for-beginners-app/src/App.js
@@ -1,26 +1,20 @@
 import { useState, useEffect } from "react";
 
-function App() {
-    const [counter, setCounter] = useState(0);
-    const [keyword, setKeyword] = useState("");
-    const onClick = () => setCounter((prev) => prev + 1);
-    const onChange = (event) => setKeyword(event.target.value);
-    console.log("I run all the time");
+function Hello() {
     useEffect(() => {
-        if (keyword !== "" && keyword.length > 5) {
-            console.log("Called The API...");
-        }
-    }, [keyword]);
+        console.log("I'm here");
+        return () => console.log("bye");
+    }, []);
+    return <h1>Hello</h1>;
+}
+
+function App() {
+    const [showing, setShowing] = useState(false);
+    const onClick = () => setShowing((prev) => !prev);
     return (
         <div>
-            <input
-                value={keyword}
-                onChange={onChange}
-                type="text"
-                placeholder="Search here..."
-            ></input>
-            <h1>{counter}</h1>
-            <button onClick={onClick}>Click me!</button>
+            {showing ? <Hello /> : null}
+            <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
         </div>
     );
 }
