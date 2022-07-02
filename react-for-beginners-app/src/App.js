70c5fb8cab6ee3c6eb2dd4a62bcd19751c46e6cb
seonhop <78877741+seonhop@users.noreply.github.com>
2022-07-02 19:45:23 -0400

    #7.2 Coin Tracker
    useEffect to run the code only at the start
    fetch()

--- react-for-beginners-app/src/App.js
+++ react-for-beginners-app/src/App.js
@@ -1,36 +1,25 @@
 import { useState, useEffect } from "react";
 
 function App() {
-    const [input, setInput] = useState("");
-    const [inputs, setInputs] = useState([]);
-    const onChange = (event) => setInput(event.target.value);
-    const onSubmit = (event) => {
-        event.preventDefault();
-        console.log(input);
-        if (input === "") {
-            return;
-        }
-        setInput("");
-        setInputs((currArray) => [input, ...currArray]);
-    };
-    console.log(inputs);
-
+    const [loading, setLoading] = useState(true);
+    const [coins, setCoins] = useState([]);
+    useEffect(() => {
+        fetch("https://api.coinpaprika.com/v1/tickers")
+            .then((response) => response.json())
+            .then((json) => {
+                setCoins(json);
+                setLoading(false);
+            });
+    }, []);
     return (
         <div>
-            <h1>My To-Do's {inputs.length}</h1>
-            <form onSubmit={onSubmit}>
-                <input
-                    value={input}
-                    onChange={onChange}
-                    type="text"
-                    placeholder="Write your To-do"
-                />
-                <button>Add To Do</button>
-            </form>
-            <hr />
+            <h1>The Coins! ({coins.length})</h1>
+            {loading ? <strong>Loading...</strong> : null}
             <ul>
-                {inputs.map((item, index) => (
-                    <li key={index}>{item}</li>
+                {coins.map((coin) => (
+                    <li key={coin.id}>
+                        {coin.name} ({coin.symbol}: ${coin.quotes.USD.price})
+                    </li>
                 ))}
             </ul>
         </div>
