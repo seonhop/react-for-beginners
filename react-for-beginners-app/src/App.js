09a90cf845fa739b36b3ad0579c45ef263e2c69d
seonhop <78877741+seonhop@users.noreply.github.com>
2022-07-02 14:34:56 -0400

    #6.2 Deps
    #Specifying a specific element to look for a change and render only if there was a change in that specific element
    
    useEffect(f, [elem]);

--- react-for-beginners-app/src/App.js
+++ react-for-beginners-app/src/App.js
@@ -2,13 +2,23 @@ import { useState, useEffect } from "react";
 
 function App() {
     const [counter, setCounter] = useState(0);
+    const [keyword, setKeyword] = useState("");
     const onClick = () => setCounter((prev) => prev + 1);
+    const onChange = (event) => setKeyword(event.target.value);
     console.log("I run all the time");
     useEffect(() => {
-        console.log("Called The API...");
-    }, []);
+        if (keyword !== "" && keyword.length > 5) {
+            console.log("Called The API...");
+        }
+    }, [keyword]);
     return (
         <div>
+            <input
+                value={keyword}
+                onChange={onChange}
+                type="text"
+                placeholder="Search here..."
+            ></input>
             <h1>{counter}</h1>
             <button onClick={onClick}>Click me!</button>
         </div>
