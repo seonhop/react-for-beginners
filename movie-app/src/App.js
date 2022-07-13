d8d30e6868d1e773471619f1d0b30524b0a6cefb
seonhop <78877741+seonhop@users.noreply.github.com>
2022-07-13 12:09:55 -0400

    #7.4 Movie App part two
    #installing react-router-dom
    npm i react-router-dom
    
    Create folders for routes and components

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,22 +1,5 @@
-import { useEffect, useState } from "react";
-
 function App() {
-    const [loading, setLoading] = useState(true);
-    const [movies, setMovies] = useState([]);
-    const getMovies = async () => {
-        const json = await (
-            await fetch(
-                `https://yts.mx/api/v2/list_movies.jsonminimum_rating=8.8&sort_by=year`
-            )
-        ).json();
-        setMovies(json.data.movies);
-        setLoading(false);
-    };
-    useEffect(() => {
-        getMovies();
-    }, []);
-    console.log(movies);
-    return <div>{loading ? <h1>Loading...</h1> : null}</div>;
+    return null;
 }
 
 export default App;
